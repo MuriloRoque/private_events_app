d7bec2a30d57b2574c3b05aebefd18dad7e2beb4
Luckyaremu <aremu.baba70@gmail.com>
2020-05-14 15:54:12 +0100

    forms created for signin and signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,11 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.find_by(params[:email])
+    if @user && @user.email == params[:email]
+        session[:user_id] = @user.id
+        flash[:success] = "You have successfully logged in."
+        redirect_to root_path
   end
-
+end
 end
